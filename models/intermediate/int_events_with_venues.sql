815c26290654399aa7a99f90b9cafd51fe6946eb
glopreto <you@example.com>
2025-05-19 17:39:27 +0100

    Add int_events_with_venues model with version tracking and status flags

--- models/intermediate/int_events_with_venues.sql
+++ models/intermediate/int_events_with_venues.sql
@@ -6,21 +6,40 @@ venues as (
     select * from {{ ref('stg_venues') }}
 ),
 
+-- Add row number to track event versions but keep all records
+event_versions as (
+    select 
+        *,
+        row_number() over (
+            partition by group_id, event_name, event_start_time, venue_id 
+            order by event_created_at desc
+        ) as event_version_number
+    from events
+),
+
 event_with_keys as (
     select
         -- Surrogate keys
-        {{ dbt_utils.generate_surrogate_key(['events.group_id', 'events.event_name']) }} as event_sk
+        {{ dbt_utils.generate_surrogate_key(['event_versions.group_id', 'event_versions.event_name', 'event_versions.event_start_time', 'event_versions.venue_id']) }} as event_sk
         , {{ dbt_utils.generate_surrogate_key(['venues.venue_id']) }} as venue_sk
         
         -- Event information
-        , events.group_id
-        , events.event_name
-        , events.event_description
-        , events.event_created_at
-        , events.event_start_time
-        , events.event_duration_seconds
-        , events.rsvp_limit
-        , events.event_status
+        , event_versions.group_id
+        , event_versions.event_name
+        , event_versions.event_description
+        , event_versions.event_created_at
+        , event_versions.event_start_time
+        , event_versions.event_duration_seconds
+        , event_versions.rsvp_limit
+        , event_versions.event_status
+        , event_versions.event_version_number  -- Include version number for use in marts layer
+        
+        -- Event status flags for easier filtering
+        , case when event_versions.event_status = 'suggested' then true else false end as is_suggested
+        , case when event_versions.event_status = 'proposed' then true else false end as is_proposed
+        , case when event_versions.event_status = 'cancelled' then true else false end as is_cancelled
+        , case when event_versions.event_status = 'past' then true else false end as is_past
+        , case when event_versions.event_status = 'upcoming' then true else false end as is_upcoming
         
         -- Venue information
         , venues.venue_id
@@ -29,9 +48,9 @@ event_with_keys as (
         , venues.country as venue_country
         , venues.lat as venue_lat
         , venues.lon as venue_lon
-    from events
+    from event_versions
     left join venues
-        on events.venue_id = venues.venue_id
+        on event_versions.venue_id = venues.venue_id
 )
 
 select * from event_with_keys
